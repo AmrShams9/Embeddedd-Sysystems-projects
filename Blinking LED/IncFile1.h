4e112cc1f0d9815d29210d0281997931aa061bda
Amr Shams <102551102+AmrShams9@users.noreply.github.com>
2023-04-20 18:11:19 +0200

    Add files via upload

---
+++ Blinking LED/IncFile1.h
@@ -0,0 +1,16 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 4/20/2023 5:04:10 PM
+ *  Author: amrkh
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+
+
+
+
+#endif /* INCFILE1_H_ */
---
+++ Blinking LED/main.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#define F_CPU 1000000UL  // set the clock frequency to 1 MHz
+#include <util/delay.h>
+
+   // include the avr/io.h header file
+
+#define LED_PIN 13    // define the LED pin as 13
+
+int main(void)
+{
+	DDRB |= (1 << LED_PIN);  // set the LED pin as an output
+	while (1) {              // loop forever
+		PORTB |= (1 << LED_PIN); // turn on the LED
+		_delay_ms(1000);      // wait for 1 second
+		PORTB &= ~(1 << LED_PIN); // turn off the LED
+		_delay_ms(1000);      // wait for 1 second
+	}
+	return 0;
+}
+
